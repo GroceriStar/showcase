41b77e4891bf05168cd9be2cdb659b030718053c
Arthur Tkachenko <atherdon@gmail.com>
2018-12-17 04:19:25 +0200

    +++

--- src/components/DepartmentInfo/DepartmentInfo.js
+++ src/components/DepartmentInfo/DepartmentInfo.js
@@ -2,7 +2,6 @@ import React, {
   Component
 } from 'react'
 
-
 import { IngredientList } from '@groceristar/grocery-component'
 
 // import uuidv1 from 'uuid/v1';
--- src/views/GroceryView.js
+++ src/views/GroceryView.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 
 // if we want to use antd styles we should include them
-import "antd/dist/antd.css";
+// import "antd/dist/antd.css";
 
 import { DepartmentList } from '@groceristar/grocery-component'
 import DepartmentListCollapse from '../components/DepartmentList/DepartmentListCollapse'
