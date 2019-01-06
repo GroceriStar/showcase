d8cbf918ec86b7e640f40820ddc6a1cbf791e0e2
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-06 15:39:00 +0200

    +

--- src/components/Recipe/FullDetails.js
+++ src/components/Recipe/FullDetails.js
@@ -1,3 +1,4 @@
+import React, { Component, Fragment } from 'react'
 const FullDetails = () => {
 
 }
--- src/components/Recipe/ShortDetails.js
+++ src/components/Recipe/ShortDetails.js
@@ -1,3 +1,5 @@
+import React, { Component, Fragment } from 'react'
+
 const ShortDetails = () => {
 
 }
