870071046699231c817174f0fd1d3e797b19953e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-01-10 01:25:27 +0200

    +

--- src/components/GroceryListToPDF/RenderLists/RenderLists.js
+++ src/components/GroceryListToPDF/RenderLists/RenderLists.js
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types';
 import { Text, View, StyleSheet } from '@react-pdf/renderer';
 import List, { Item } from '../List/List';
 
+
+
+
 // @TODO move this selector away from this not a container like component.
 import { getFullGrocery } from "../../../selectors/selector";
 
--- src/components/RecipeToPDF/index.js
+++ src/components/RecipeToPDF/index.js
@@ -1,7 +1,13 @@
+
+
+
+
 import DownloadPDFLink   from './DownloadPDFLink/DownloadPDFLink';
 import GeneratePDFButton from './GeneratePDFButton/GeneratePDFButton';
 import DocumentLayout    from './DownloadPDFLink/DocumentLayout';
 import RenderLists       from './RenderLists/RenderLists';
+
+
 
 
 import { RenderLists3 } from '@groceristar/pdf-export';
