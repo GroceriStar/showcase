ac3fe1847da26163b5d8994a082d4c7ef41a1edd
Arthur Tkachenko <atherdon@gmail.com>
2019-01-31 21:59:50 +0200

    +++

--- src/components/PrettyPrintJSON/PrettyPrintJson.js
+++ src/components/PrettyPrintJSON/PrettyPrintJson.js
@@ -4,14 +4,12 @@ import React from 'react';
 // we can also add highlight.js and make it better.
 // https://stackoverflow.com/a/35340052
 
-
-
 class PrettyPrintJson extends React.Component {
-    render() {
-         // data could be a prop for example
-         const { data } = this.props;
-         return (<div><pre>{JSON.stringify(data, null, 2) }</pre></div>);
-    }
+  render() {
+    // data could be a prop for example
+    const { data } = this.props;
+    return (<div><pre>{JSON.stringify(data, null, 2) }</pre></div>);
+  }
 }
 // Usage:
 // <PrettyPrintJson data={data} />
@@ -19,13 +17,13 @@ class PrettyPrintJson extends React.Component {
 
 
 class JsonDump extends React.Component {
-    static propTypes = {
-        children: React.PropTypes.any
-    }
+  static propTypes = {
+    children: React.PropTypes.any
+  }
 
-    render() {
-        return <pre>{JSON.stringify(this.props.children, null, 4)}</pre>
-    }
+  render() {
+    return <pre>{JSON.stringify(this.props.children, null, 4)}</pre>
+  }
 }
 
 // Usage:
