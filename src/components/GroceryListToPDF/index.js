5cbc17ec23bb6536b71f822fa8beca4a1a01e00e
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-10 23:03:01 +0200

    +

--- src/components/GroceryListToPDF/index.js
+++ src/components/GroceryListToPDF/index.js
@@ -1,3 +1,13 @@
+// in order to make transition more smooth,
+// we'll use this file similar to our selectors.js
+
+// import { DownloadPDFLink, DownloadPDFLink2, DownloadPDFLink3 }  from '@groceristar/pdf-export';
+// import { DocumentLayout, DocumentLayout2, DocumentLayout3 }  from '@groceristar/pdf-export';
+// import { RenderLists, RenderLists2, RenderLists3 }  from '@groceristar/pdf-export';
+//------------
+
+
+
 import DownloadPDFLink   from './DownloadPDFLink/DownloadPDFLink';
 import DownloadPDFLink2   from './DownloadPDFLink2/DownloadPDFLink2';
 import DownloadPDFLink3   from './DownloadPDFLink3/DownloadPDFLink3';
