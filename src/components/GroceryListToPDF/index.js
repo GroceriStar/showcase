ddb997db76bd6b7cd5df1bdf55778dc3b2d76a42
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-18 07:19:44 +0200

    clean up

--- src/components/GroceryListToPDF/index.js
+++ src/components/GroceryListToPDF/index.js
@@ -1,45 +1,23 @@
 // in order to make transition more smooth,
 // we'll use this file similar to our selectors.js
 
-// import { DownloadPDFLink, DownloadPDFLink2, DownloadPDFLink3 }  from '@groceristar/pdf-export';
-// import { DocumentLayout, DocumentLayout2, DocumentLayout3 }  from '@groceristar/pdf-export';
-// import { RenderLists, RenderLists2, RenderLists3 }  from '@groceristar/pdf-export';
+import { DownloadPDFLink
+   }  from '@groceristar/pdf-export';
+// import { DocumentLayout }  from '@groceristar/pdf-export';
+import { RenderLists1, RenderLists2, RenderLists3 }  from '@groceristar/pdf-export';
 //------------
-
-
-
-import DownloadPDFLink    from './DownloadPDFLink/DownloadPDFLink';
-import DownloadPDFLink2   from './DownloadPDFLink2/DownloadPDFLink2';
-import DownloadPDFLink3   from './DownloadPDFLink3/DownloadPDFLink3';
-
-
-// import DocumentLayout    from './DownloadPDFLink/DocumentLayout';
-//
-// import DocumentLayout2    from './DownloadPDFLink2/DocumentLayout2';
-//
-// import DocumentLayout3    from './DownloadPDFLink3/DocumentLayout3';
-
-
-import RenderLists       from './RenderLists/RenderLists';
-
-
-
-import RenderLists2       from './RenderLists2/RenderLists2';
-// import RenderLists3       from './RenderLists3/RenderLists3';
-
-import { RenderLists3 }  from '@groceristar/pdf-export';
 
 
 export {
   DownloadPDFLink,
-  DownloadPDFLink2,
-  DownloadPDFLink3,
+  // DownloadPDFLink2,
+  // DownloadPDFLink3,
 
   // DocumentLayout,
   // DocumentLayout2,
   // DocumentLayout3,
 
-  RenderLists,
+  RenderLists1,
 
   RenderLists2,
   RenderLists3
