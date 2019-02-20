55de320113c9517c2e9648bb4164a99357eaf43b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-02-20 20:34:24 +0200

    +

---
+++ src/components/gridididi.js
@@ -0,0 +1,29 @@
+// import { List, Card } from 'antd';
+//
+// const data = [
+//   {
+//     title: 'Title 1',
+//   },
+//   {
+//     title: 'Title 2',
+//   },
+//   {
+//     title: 'Title 3',
+//   },
+//   {
+//     title: 'Title 4',
+//   },
+// ];
+//
+// ReactDOM.render(
+//   <List
+//     grid={{ gutter: 16, column: 4 }}
+//     dataSource={data}
+//     renderItem={item => (
+//       <List.Item>
+//         <Card title={item.title}>Card content</Card>
+//       </List.Item>
+//     )}
+//   />,
+//   mountNode
+// );
---
+++ src/components/nodatatatata.js
@@ -0,0 +1,24 @@
+// import { Empty } from 'antd';
+//
+// ReactDOM.render(
+//   <Empty />,
+//   mountNode
+// );
+//
+// import { Empty, Button } from 'antd';
+//
+// ReactDOM.render(
+//   <Empty
+//     image="https://gw.alipayobjects.com/mdn/miniapp_social/afts/img/A*pevERLJC9v0AAAAAAAAAAABjAQAAAQ/original"
+//     description={
+//       <span>
+//         Customize <a href="#API">Description</a>
+//       </span>
+//     }
+//   >
+//     <Button type="primary">Create Now</Button>
+//   </Empty>,
+//   mountNode
+// );
+//
+// https://ant.design/components/empty/#components-empty-demo-config-provider
