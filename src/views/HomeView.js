25ad6f97161bc97ed9978155b99a49dc13b42382
Vadim <vadim.putrov@gmail.com>
2019-03-08 10:58:22 +0200

    add Empty

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -4,9 +4,7 @@ import {
   Menu
 } from '@groceristar/antd-showcase-components';
 
-
 import Cell     from "./../components/Cell/Cell";
-
 
 import { Grid } from "mauerwerk";
 import _        from "lodash";
@@ -14,20 +12,18 @@ import _        from "lodash";
 import "antd/dist/antd.css";
 import "./../styles.css";
 
-
 import {
   EmptyImageWrapper,
   LinkEmptyImage
 } from '@groceristar/antd-showcase-components';
 
-
+import { Empty } from "antd"
 // @TODO i want to have a separation.
 // right now to grid we're passing all stuff that we have for grocery lists,
 // but we only need major things like id and title, usually.
 // it's better to fetch additional data, related to one single list only when we open a tile...
 // it's also not necessary, becausee we're passing only id to InsideLayout component.
 // but what we should pass into InsideLayout it's also debatable. because we're overweight it right now
-
 
 import { getGroceryCollection } from "./../selectors/selector";
 
@@ -69,14 +65,10 @@ class HomeView extends Component {
         data: response.data
       })
     } catch (e) {
+        console.log(this.state.data);
         console.error("Can't connect to Database in HomeView");
     }
   }
-
-
-
-
-
 
     //@TODO move this stuff to header. It's in separated package, btw ;)
     search  = e => this.setState({ filter: e.target.value });
@@ -87,12 +79,8 @@ class HomeView extends Component {
     setMargin  = e => this.setState({ margin: parseInt(e.key, 10) });
     setHeight  = e => this.setState({ height: e });
 
-
-
-
     grocery   = [];
     currentId = 1;
-
 
     // @TODO change this. I really don't like this method
     getId(name){
@@ -125,11 +113,42 @@ class HomeView extends Component {
         return id;
     }
 
-
+    getGrid(){
+      if ( this.state.data.length == 0 ){
+        return (<Empty />)
+      }else{
+        return (
+          <Grid
+            className="grid"
+            // Arbitrary data, should contain keys, possibly heights, etc.
+            data={this.state.data}
+            // Key accessor, instructs grid on how to fet individual keys from the data set
+            keys={d => d.name}
+            // Can be a fixed value or an individual data accessor
+            heights={this.state.height ? d => d.height : 200} //@TODO update it L(
+            // Number of columns
+            columns={this.state.columns}
+            // Space between elements
+            margin={this.state.margin}
+            // Removes the possibility to scroll away from a maximized element
+            lockScroll={true}
+            // Delay when active elements (blown up) are minimized again
+            closeDelay={400}
+          >
+            {(data, open, toggle) => (
+                <Cell {...data}
+                  open={open}
+                  toggle={toggle}
+                  id={this.getId(this.state.data.name)}
+                  type='grocery-list'
+                />
+            )}
+          </Grid>
+        )
+      }
+    }
 
   render() {
-    console.log("State");
-    console.log(this.state.data);
     // console.log(this.state.data);
     //@TODO i don't like this structure...
     // we can use streight map or lodash map and have more recognizible logic.
@@ -143,13 +162,10 @@ class HomeView extends Component {
     // const data = this.state.data;
 
     return (
-
       <div className="main">
-
         <Menu />
 
-
-        <HeaderShowcase
+        <HaderShowcase
           {...this.state}
           search={this.search}
           shuffle={this.shuffle}
@@ -158,37 +174,8 @@ class HomeView extends Component {
           setHeight={this.setHeight}
         />
 
-
-        {/* if ( this.state.data )  */}
-
-        <Grid
-          className="grid"
-          // Arbitrary data, should contain keys, possibly heights, etc.
-          data={this.state.data}
-          // Key accessor, instructs grid on how to fet individual keys from the data set
-          keys={d => d.name}
-          // Can be a fixed value or an individual data accessor
-          heights={this.state.height ? d => d.height : 200} //@TODO update it L(
-          // Number of columns
-          columns={this.state.columns}
-          // Space between elements
-          margin={this.state.margin}
-          // Removes the possibility to scroll away from a maximized element
-          lockScroll={true}
-          // Delay when active elements (blown up) are minimized again
-          closeDelay={400}
-        >
-          {(data, open, toggle) => (
-              <Cell {...data}
-                open={open}
-                toggle={toggle}
-                id={this.getId(this.state.data.name)}
-                type='grocery-list'
-              />
-          )}
-        </Grid>
+        {this.getGrid()}
       </div>
-
     );
   }
 }
